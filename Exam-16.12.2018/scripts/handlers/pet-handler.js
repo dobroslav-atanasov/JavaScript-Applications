094d56df26f8d56a0d1847dca7dd43cd5fd0c972
dobroslav-atanasov <dobroslav_atanasov@abv.bg>
2019-04-10 17:40:12 +0300

    Updated dashboard template.

--- Exam-16.12.2018/scripts/handlers/pet-handler.js
+++ Exam-16.12.2018/scripts/handlers/pet-handler.js
@@ -1,12 +1,20 @@
 handlers.getAllPets = function (context) {
     context.isAuth = userService.isAuth();
     context.username = sessionStorage.getItem('username');
-    
+
     context.loadPartials({
         header: './templates/header.hbs',
-        footer: './templates/footer.hbs'
+        footer: './templates/footer.hbs',
+        pet: './templates/pet.hbs'
     }).then(function () {
-        this.partial('./templates/dashboard.hbs');
+        petService.getAllPets()
+            .then((pets) => {
+                let otherPets = pets.filter(x => x._acl.creator !== sessionStorage.getItem('userId'));
+                context.pets = otherPets;
+                this.partial('./templates/dashboard.hbs');
+            }).catch(function (err) {
+                console.log(err);
+            });
     }).catch(function (err) {
         console.log(err);
     });
